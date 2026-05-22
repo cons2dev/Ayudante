ba1f18c00a730e0bc57c536082ebe4eb8206fd4f
ssamaniego <amatxucool@gmail.com>
2026-05-22 10:09:21 +0200

    3er commit

---
+++ visualizaciones/correlacion.py
@@ -0,0 +1,24 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def heatmap_corr(df):
+    sns.set_theme(style="white")
+
+    numeric_df = df.select_dtypes(include=["number", "bool"])
+
+    # Compute the correlation matrix
+    corr = numeric_df.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(11, 9))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, mask=mask, annot=True, cmap=cmap, vmax=.3, center=0,
+                square=True, linewidths=.5, cbar_kws={"shrink": .5})
